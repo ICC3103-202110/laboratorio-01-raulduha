88c806faa09253608db62f2ac6a3e60a10ab274e
Raúl Duhalde <RaulDuha@MacBook-Air-de-AirMac.local>
2021-03-26 16:52:26 -0300

    traduccion_ingles

--- Lab1.py
+++ Lab1.py
@@ -7,7 +7,7 @@ print('')
 
 matrix = []
 
-pair_of_cards = int(input('cual es el numero de cartas a jugar?: '))
+pair_of_cards = int(input('how many pair of cards do you want?: '))
 print('')
 player1 = 0
 player2 = 0 
@@ -22,7 +22,7 @@ game_list_aleatory = random.sample(game_list, len(game_list))
 
 #Pasamos de lista a matrix
 game_matrix = np.array(game_list_aleatory).reshape(2, pair_of_cards)
-print(game_matrix)
+#print(game_matrix)
 
 #creamos una matrix que contendra los '*'
 def hidden_matrix(filas, n):
@@ -64,7 +64,7 @@ def show_matrix2(x1, y1, x2, y2):
         result[x1][y1] = ('*')
         result[x2][y2] = ('*')
     print('')
-    print('TABLERO ACTUAL')
+    print('ACTUAL BOARD')
     return result
 
 #chequeamos que las coordenadas no se salgan del tablero
@@ -80,10 +80,10 @@ while total_score != pair_of_cards:
 
         if turn%2 == 0:
 
-            print('juega Jugador 1: ')
-            x1_player1 = int(input('cual es tu x1= '))
-            y1_player1 = int(input('cual es tu y1= '))
-            print('La carta es un: ',game_matrix[x1_player1][y1_player1])
+            print('Turn of player 1: ')
+            x1_player1 = int(input('what is your x1= '))
+            y1_player1 = int(input('what is your y1= '))
+            print('The card is a: ',game_matrix[x1_player1][y1_player1])
             print('')
             print(show_matrix1(x1_player1,y1_player1))
             print('')
@@ -91,8 +91,8 @@ while total_score != pair_of_cards:
             x2_player1 = int(input('cual es tu x2= '))
             y2_player1 = int(input('cual es tu y2= '))
 
-            print('La primera carta es un: ', game_matrix[x1_player1][y1_player1])
-            print('La segunda carta es un: ', game_matrix[x2_player1][y2_player1])
+            print('The first card is a: ', game_matrix[x1_player1][y1_player1])
+            print('The second card is a: ', game_matrix[x2_player1][y2_player1])
             
             print(show_matrix2(x1_player1,y1_player1,x2_player1,y2_player1))
             print('')
@@ -101,52 +101,52 @@ while total_score != pair_of_cards:
                 player1 += 1
                 total_score = player1 + player2
 
-                print('haz acertado, el jugador 1 tiene ahora', player1, 'puntos')
+                print('you guessed, player 1 has now', player1, 'points')
                   
             else: 
-                print('no acertaste, el jugador 1 tiene',player1, ' puntos')
+                print('you did not guess, player 1 has ',player1, ' points')
                 
             
 
         elif turn%2 != 0:
-            print('juega Jugador 2: ')
-            x1_player2 = int(input('cual es tu x1= '))
-            y1_player2 = int(input('cual es tu y1= '))
+            print('Turn of player 2: ')
+            x1_player2 = int(input('what is your x1= '))
+            y1_player2 = int(input('what is your y1= '))
 
-            print('La carta es un: ', game_matrix[x1_player2][y1_player2])
+            print('the card is a: ', game_matrix[x1_player2][y1_player2])
             print('')
             print(show_matrix1(x1_player2,y1_player2))
             print('')
 
-            x2_player2 = int(input('cual es tu x2= '))
-            y2_player2 = int(input('cual es tu y2= '))
+            x2_player2 = int(input('What is your x2= '))
+            y2_player2 = int(input('What is your y2= '))
 
-            print('La primera carta es un: ', game_matrix[x1_player2][y1_player2])
-            print('La segunda carta es un: ', game_matrix[x2_player2][y2_player2])
+            print('The first card is a: ', game_matrix[x1_player2][y1_player2])
+            print('The second card is a: ', game_matrix[x2_player2][y2_player2])
             print('')
             print(show_matrix2(x1_player2,y1_player2,x2_player2,y2_player2))   
             print('')
 
             if game_matrix[x1_player2][y1_player2] == game_matrix[x2_player2][y2_player2]:
                 player2  += 1
-                print('haz acertado, el jugador 2 tiene ahora', player2, 'puntos')
+                print('You guessed, player 2 has now', player2, 'points')
                
                 total_score = player1 + player2
             else: 
-                print('no acertaste, el jugador 2 tiene',player2,' puntos')
+                print('You did not guess, player 2 has',player2,' points')
                 
         
-print('jugador 1 termino con: ', player1, 'puntos')
-print('jugador 2 termino con: ', player2, 'puntos')
+print('Player 1 ended with: ', player1, 'points')
+print('Player 2 ended with: ', player2, 'points')
 print('')
 
 if player1 > player2:
-    print('jugador 1 tiene mas puntos (',player1,')')
+    print('Player 1 has more points (',player1,')')
 
 elif player1 == player2:
-    print('los jugadores empataron con (',player1,')')
+    print('The two players tied (',player1,')')
 
 else:
-    print('jugador 2 tiene mas puntos (',player2,')')
+    print('Player 2 has more points (',player2,')')
 
         
